07519dc7fbb38b227d887fe393d689ddfdc4116d
Singh-Ishu <itzshaansingh@gmail.com>
2025-05-28 20:42:09 +0530

    ADD Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+/*The component is a header nav that is available on all pages.Header
+The brand logo links to the home page.
+The search bar allows users to search for products.
+The profile icon links to the profile page
+The cart icon links to the cart page*/
+
+import "./Header.css";
+
+export default function Header() {
+    return (
+        <div className="Header">
+            <h1>EarCandy.Co</h1>
+            <div id="nav-icons-div">
+                <i className="material-icons">shopping_cart</i>
+                <i className="material-icons">person</i>
+            </div>
+        </div>
+    );
+}
