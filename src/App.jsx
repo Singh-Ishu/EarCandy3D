f9e9f9b5c801d5ac83d53b3b9b3b1569a9de360c
Singh-Ishu <itzshaansingh@gmail.com>
2025-05-31 11:11:19 +0530

    ADD ProductShowcase to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Landing from "./pages/Landing";
+import ProductShowcase from "./pages/ProductShowcase";
 
 function App() {
     return (
         <>
             <Landing />
+            <ProductShowcase />
         </>
     );
 }
