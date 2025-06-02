7627f96b0ef3af3a0e1106904b8259f1c8ad9f33
Singh-Ishu <itzshaansingh@gmail.com>
2025-06-02 18:31:03 +0530

    ADD Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+    return (
+        <div className="Footer">
+            <table>
+                <tr>
+                    <th>SHOP</th>
+                    <th>SUPPORT</th>
+                    <th>ABOUT US</th>
+                    <th>CONTACT US</th>
+                </tr>
+            </table>
+        </div>
+    );
+}
