921d77fa4099d5c328fb3e25fc8ba5789339f04f
Singh-Ishu <itzshaansingh@gmail.com>
2025-06-07 10:59:26 +0530

    ADD Hamburger Menu

---
+++ src/components/HamburgerMenu.jsx
@@ -0,0 +1,16 @@
+import "./HamburgerMenu.css";
+
+export default function HamburgerMenu() {
+    return (
+        <div className="hamburger-menu">
+            <ul>
+                <li>Home</li>
+                <li>Headphones</li>
+                <li>Speakers</li>
+                <li>Soundbars</li>
+                <li>Luxury Audio</li>
+                <li>Accessories</li>
+            </ul>
+        </div>
+    );
+}
