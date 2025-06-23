38025daef0f06e9290e769b9d3e09e8276a9ee8f
Singh-Ishu <itzshaansingh@gmail.com>
2025-06-23 23:07:53 +0530

    ADD styling

--- src/components/ShopSearch.jsx
+++ src/components/ShopSearch.jsx
@@ -1,4 +1,4 @@
-// import './ShopSearch.css';
+import "./ShopSearch.css";
 
 export default function ShopSearch() {
     return (
@@ -10,20 +10,7 @@ export default function ShopSearch() {
                     className="search-input"
                 />
                 <button className="search-button">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                    >
-                        <circle cx="11" cy="11" r="8"></circle>
-                        <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-                    </svg>
+                    <i className="material-icons">search</i>
                 </button>
             </div>
         </div>
