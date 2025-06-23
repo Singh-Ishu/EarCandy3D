86bab1a5414d2320e218fa69286de1ae027962f2
Singh-Ishu <itzshaansingh@gmail.com>
2025-06-23 22:58:51 +0530

    UPDATE UI and ADD Responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,38 +3,64 @@ The brand logo links to the home page.
 The search bar allows users to search for products.
 The profile icon links to the profile page
 The cart icon links to the cart page*/
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import HamburgerMenu from "./HamburgerMenu";
 import "./Header.css";
 
 export default function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <div className="Header">
             <div className="brand-and-options">
-                <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+                <Link to="/">
                     <h1>EARCANDY.CO</h1>
                 </Link>
                 <div className="header-options">
-                    <Link to="/shop?category=headphones" className="header-option">Headphones</Link>
-                    <Link to="/shop?category=speakers" className="header-option">Speakers</Link>
-                    <Link to="/shop?category=soundbars" className="header-option">Soundbars</Link>
-                    <Link to="/shop?category=luxury" className="header-option">Luxury Audio</Link>
-                    <Link to="/shop?category=accessories" className="header-option">Accessories</Link>
+                    <Link
+                        to="/shop?category=headphones"
+                        className="header-option"
+                    >
+                        Headphones
+                    </Link>
+                    <Link
+                        to="/shop?category=speakers"
+                        className="header-option"
+                    >
+                        Speakers
+                    </Link>
+                    <Link
+                        to="/shop?category=soundbars"
+                        className="header-option"
+                    >
+                        Soundbars
+                    </Link>
+                    <Link to="/shop?category=luxury" className="header-option">
+                        Luxury Audio
+                    </Link>
+                    <Link
+                        to="/shop?category=accessories"
+                        className="header-option"
+                    >
+                        Accessories
+                    </Link>
                 </div>
             </div>
             <div id="nav-icons-div">
-                <Link to="/search" style={{ color: 'inherit' }}>
+                <Link to="/search" style={{ color: "inherit" }}>
                     <i className="material-icons">search</i>
                 </Link>
-                <Link to="/cart" style={{ color: 'inherit' }}>
+                <Link to="/cart" style={{ color: "inherit" }}>
                     <i className="material-icons">shopping_cart</i>
                 </Link>
-                <Link to="/profile" style={{ color: 'inherit' }}>
+                <Link to="/profile" style={{ color: "inherit" }}>
                     <i className="material-icons">person</i>
                 </Link>
             </div>
             <div id="hamburger-menu">
                 <i className="material-icons">menu</i>
+
                 <HamburgerMenu />
             </div>
         </div>
