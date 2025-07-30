6d353e6a861072b946e1348e4bebad5e70bc46db
Singh-Ishu <itzshaansingh@gmail.com>
2025-07-30 15:36:28 +0530

    meow

--- src/pages/LandingBold.jsx
+++ src/pages/LandingBold.jsx
@@ -3,7 +3,7 @@ import "./LandingBold.css";
 export default function LandingBold() {
     return (
         <div className="Headphone-hero-bold">
-            
+            <h1>BOLD</h1>
         </div>
     );
 }
--- src/pages/LandingMinimal.jsx
+++ src/pages/LandingMinimal.jsx
@@ -3,7 +3,7 @@ import "./LandingMinimal.css";
 export default function LandingMinimal() {
     return (
         <div className="HeadphoneHero">
-            
+            <h1>MINIMAL</h1>
         </div>
     );
 }
